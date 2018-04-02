3a39ce1cbc5ede0d04f4e2cdc25562682362e31b
Ssuarez0 <ssuarez@2011.nhi.edu>
2018-04-01 21:24:51 -0700

    Interface Done
    Added the menu bar, a text field to display file contents, an open file
    dialog and a text block to display the frequency list. I've also begun
    adding test data.
    
    Next step: Fix the file dialogue to group everything together
    
    Steps after:
    	Feed text of file into text box
    	Upon text being loaded into the box, set a timer like setTimeout
    	and then run the analyzer at the end.
    	Make sure everything is unicode for Spanish

---
+++ spanish-nl-analyzer/Spanish-Word-Frequency-Analyzer.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace spanish_nl_analyzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void file_browse_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            //File extensions
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "*.docx|*.docx|*.doc|*.doc|*.txt|*.txt";
+
+            //Display
+            Nullable<bool> result = dlg.ShowDialog();
+
+            //Process dialogue box results
+            if (result == true)
+            {
+                //Open document
+                string filename = dlg.FileName;
+                file_contents.Text = filename;
+            }
+        }
+
+        private void exit_menu_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
