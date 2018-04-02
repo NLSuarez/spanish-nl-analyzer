bb7e9a60ec5cc0add602fd6a2611b20924ec7324
Ssuarez0 <ssuarez@2011.nhi.edu>
2018-04-02 16:06:34 -0700

    Timer Implemented
    Analysis will take place on text change of text box in case someone
    wants to copy paste spanish into it instead of loading a file. This may
    change to a button in the future depending on testing. For now, we'll
    stick with the timer.

--- spanish-nl-analyzer/Spanish-Word-Frequency-Analyzer.xaml.cs
+++ spanish-nl-analyzer/Spanish-Word-Frequency-Analyzer.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,6 +21,7 @@ namespace spanish_nl_analyzer
     /// </summary>
     public partial class MainWindow : Window
     {
+        System.Windows.Threading.DispatcherTimer _typingTimer;
         public MainWindow()
         {
             InitializeComponent();
@@ -32,7 +34,7 @@ namespace spanish_nl_analyzer
 
             //File extensions
             dlg.DefaultExt = ".txt";
-            dlg.Filter = "*.docx|*.docx|*.doc|*.doc|*.txt|*.txt";
+            dlg.Filter = "*.docx, *.doc, *.txt|.docx;*.doc;*.txt|All Files|*.*";
 
             //Display
             Nullable<bool> result = dlg.ShowDialog();
@@ -40,9 +42,31 @@ namespace spanish_nl_analyzer
             //Process dialogue box results
             if (result == true)
             {
-                //Open document
+                //Determine document extension and process accordingly
                 string filename = dlg.FileName;
-                file_contents.Text = filename;
+                string ext = System.IO.Path.GetExtension(filename);
+
+                if (ext == ".txt")
+                {
+                    //If plain text, just go ahead and put it in the box.
+                    string text;
+                    using (Stream stream = dlg.OpenFile())
+                    {
+                        StreamReader reader = new StreamReader(stream);
+                        text = reader.ReadToEnd();
+                    }
+                    file_contents.Text = text;
+                } else if (ext == ".docx")
+                {
+
+                } else if (ext == ".doc")
+                {
+
+                } else
+                {
+                    //Do nothing or say invalid.
+                }
+                
             }
         }
 
@@ -51,9 +75,30 @@ namespace spanish_nl_analyzer
             Application.Current.Shutdown();
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private void file_contents_TextChanged(object sender, TextChangedEventArgs e)
         {
+            if (_typingTimer == null)
+            {
+                _typingTimer = new System.Windows.Threading.DispatcherTimer();
+                _typingTimer.Interval = TimeSpan.FromMilliseconds(2000);
+                _typingTimer.Tick += new EventHandler(this.handleTypingTimerTimeout);
+            }
+            _typingTimer.Stop(); //Reset timer
+            _typingTimer.Tag = (sender as TextBox).Text;
+            _typingTimer.Start();
+        }
 
+        private void handleTypingTimerTimeout(object sender, EventArgs e)
+        {
+            var timer = sender as System.Windows.Threading.DispatcherTimer;
+            if (timer == null)
+            {
+                return;
+            }
+
+            string textToAnalyze = timer.Tag.ToString();
+            results_box.Text = textToAnalyze;
+            timer.Stop(); //Stop the timer so we act only once per keystroke
         }
     }
 }
